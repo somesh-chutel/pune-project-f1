48eeb58550cb3980e3f54836e980be64cb818ba6
srocknik <somesh.chutel90@gmail.com>
2024-06-26 11:18:30 +0530

    third commit

--- src/components/jobs/index.jsx
+++ src/components/jobs/index.jsx
@@ -1,79 +1,95 @@
-import Header from '../header';
-import DisplayJobs from '../displayJobs';
-import FilterSection from '../filterSection';
-import Cookies from 'js-cookie';
-import { useEffect, useState } from 'react';
-import './index.css';
+import Header from "../header";
+import DisplayJobs from "../displayJobs";
+import FilterSection from "../filterSection";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import "./index.css";
 
-const Jobs = ()=> {
-    const [allValues,setValues] = useState({
-        jobsList : []
-    });
+const Jobs = () => {
+  const [allValues, setValues] = useState({
+    jobsList: [],
+    empType: [],
+    sallryRange: "",
+    userSearch: "",
+  });
 
-    const token = Cookies.get("jwtToken");
+  const token = Cookies.get("jwtToken");
 
-    useEffect(()=>{
+  useEffect(() => {
 
-        const fetchJobsData = async ()=>{
+    console.log(allValues.empType);
 
-            const api = "https://apis.ccbp.in/jobs";
+    const fetchJobsData = async () => {
+      const api = `https://apis.ccbp.in/jobs?employment_type=${allValues.empType}&minimum_package=${allValues.sallryRange}&search=${allValues.userSearch}`;
 
-            const options = {
-                method: 'GET',
-                headers : {
-                    Authorization : `Bearer ${token}`
-                }
-              }
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await fetch(api, options);
+
+      const data = await response.json();
+
+      if (response.ok === true) {
+        setValues({ ...allValues, jobsList: data.jobs });
+      }
+
+    };
+
+    fetchJobsData();
+  }, [allValues.userSearch,allValues.empType]);
+
+  const onChangeUserIn = (e) => {
+    if (e.key === "Enter") {
+      setValues({ ...allValues, userSearch: e.target.value });
+    }
+  };
+
+  const retriveEmpValue = (value,isCheked)=>{
+
+    if(isCheked === true){
+
+        setValues({...allValues,empType:[...allValues.empType,value]});
+        
+    }
+    else{
+
+        setValues({...allValues,empType: allValues.empType.filter(each=> each !== value ) })
+
+    }
 
 
-              const response = await fetch(api,options);
+    
 
-              const data = await response.json();
+  }
 
-              console.log(data.jobs);
+  return (
+    <div>
+      <Header />
 
-              if(response.ok===true){
-                setValues({...allValues,jobsList : data.jobs})
-              }
-
-        }
-
-
-        fetchJobsData();
-
-
-    },[]);
-
-
-    return (
-
-        <div>
-            <Header/>
-
-            <div className='filter-display-all-jobs-cont'>
-                    <div className='filter-section-cont'>
-                        <FilterSection/>
-                    </div>
-                    <div className='display-all-jobs-cont'>
-                        <input placeholder='Search' type="search" className='form-control my-input'/>
-                        <ul className='jobs-list-cont'>
-                                {
-                                   allValues.jobsList.map(each=>
-
-                                    <DisplayJobs jobsItem = {each} key={each.id}/>
-                                   ) 
-                                }
-
-                        </ul>
-                        
-                    </div>
-            </div>
-
+      <div className="filter-display-all-jobs-cont">
+        <div className="filter-section-cont">
+          <FilterSection empTypeFunct = {retriveEmpValue}/>
         </div>
-    )
-}
-
-
-
+        <div className="display-all-jobs-cont">
+          <input
+            onKeyDown={onChangeUserIn}
+            placeholder="Search"
+            type="search"
+            className="form-control my-input"
+          />
+          <ul className="jobs-list-cont">
+            {allValues.jobsList.map((each) => (
+              <DisplayJobs jobsItem={each} key={each.id} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Jobs;
